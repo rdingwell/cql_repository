094ed21bc595b807ffda2b80449a9f285e3dae5f
Rob Dingwell <bobd@mitre.org>
2015-06-03 14:27:12 -0400

    ugly first implmentation of document tests

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,8 @@
+
 class Document
   include Mongoid::Document
   include Mongoid::Timestamps
-  include DocumentValidators
+ 
 
   field :data, type: String
   field :library, type: String
@@ -13,17 +14,32 @@ class Document
   validates_presence_of :data
   validates_presence_of :library
   # before create validate only lib/version combo
-  validates_unique_library on: :create
-  # before save validate dependencies exist
-  validates_dependencies_exist
-  # before delete check dependencies
-  before_destroy :validates_not_a_dependency
+  validate({on: :create}) do |record|
+    if Document.where(library: record.library, version: record.version).count > 0
+      record.errors.add(:base, "Another library was found with the same identifier and version")
+    end
+  end
+
+  # # before save validate dependencies exist
+  validate do|record|
+    record.dependencies.each_pair do |k,v|
+      if Document.where(library: k, version: v).count == 0
+        record.errors.add(:base, "Could not find dependency #{k}/#{v}")
+      end
+    end
+  end
+
+  before_destroy do |record|
+    if Document.where(library: record.library, version: record.version).count > 0
+      raise  "Document is used as a dependency for another document"
+    end
+  end
   
 
   def self.parse_dependencies(cql)
     deps = {}
     cql.lines.each do |l|
-      line = l.trim
+      line = l.strip
       if line.match /^include/
           parts = line.split
           if parts.length == 4
@@ -31,7 +47,7 @@ class Document
             deps[parts[1]] = nil
           elsif parts.length == 6
             # has version info
-             deps[parts[1]] = parts[3]
+             deps[parts[1]] = parts[3].gsub("'", "")
           end
       end
     end
@@ -43,22 +59,35 @@ class Document
     version = nil
 
     cql.lines.each do |l|
-      line = l.trim
+      line = l.strip
       if line.match /^library/
           parts = line.split
           if parts.length == 2
             library = parts[1]
           elsif parts.length == 4
             library = parts[1]
-            version = parts[3]
+            version = parts[3].gsub("'", "")
           end
       end
     end
-    library,version
+    [library,version]
+  end
+
+
+  def self.find_or_create_by_cql(cql)
+    library,version = parse_library_and_version(cql)
+    document = Document.where(library: library, version: version).first
+    unless document
+      document = Document.new(library: library, 
+                              version: version, data: cql, 
+                              dependencies: parse_dependencies(cql))
+      document.save
+    end
+    document
   end
 
   def find_dependencies
-    crit = self.where
+    crit = Document.all
     self.dependencies.each_pair do |k,v|
       crit = crit.or(library: k, version: v)
     end
@@ -66,7 +95,7 @@ class Document
   end
 
   def find_dependents
-    self.where(library: self.library, version: self.version)
+    Document.where("dependencies.#{library}" => version)
   end
 
 end
--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,36 +1,108 @@
 require 'test_helper'
 
 class DocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
-  test "should validate unique library/version" do
-    assert false
+  def setup
+    Document.collection.drop
   end
 
+
+  test "should validate unique library/version" do
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_CDS.cql")
+    doc = Document.find_or_create_by_cql(cql)
+    assert doc.errors.empty?
+
+    library,version = Document.parse_library_and_version(cql)
+    doc = Document.new(library: library, 
+                            version: version, data: cql, 
+                             dependencies: Document.parse_dependencies(cql))
+    doc.save
+
+    assert !doc.errors.empty?
+  end
+
+  test "should validate precense of library identifier" do
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_CDS.cql")
+    library,version = Document.parse_library_and_version(cql)
+    doc = Document.new(library: library, 
+                            version: version, data: cql, 
+                             dependencies: Document.parse_dependencies(cql))
+    doc.library = nil
+    doc.save
+
+    assert !doc.errors.empty?
+  end
+
+
   test "should validate dependencies" do
-    assert false
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_CDS_UsingCommon.cql")
+    doc = Document.find_or_create_by_cql(cql)
+    assert !doc.errors.empty?
+
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_Common.cql")
+    doc = Document.find_or_create_by_cql(cql)
+    doc.save
+    assert doc.errors.empty?
+
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_CDS_UsingCommon.cql")
+    doc = Document.find_or_create_by_cql(cql)
+    assert doc.errors.empty? , "There should be no errors dependencies should have validated"
   end
 
   test "should parse dependencies" do
-    assert false
+    cql = File.read("test/fixtures/cql_documents/include.cql")
+    deps = Document.parse_dependencies(cql)
+    assert_equal deps, {"withVersion" => "2","withoutVersion"=>nil}
+
   end
 
   test "should parse library and version" do
-    assert false
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_CDS.cql")
+    identifier,version = Document.parse_library_and_version(cql)
+    assert_equal identifier, "ChlamydiaScreening_CDS"
+    assert_equal version, "2"
   end
 
   test "should check for dependent documents on delete" do
-    assert false
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_Common.cql")
+    doc1 = Document.find_or_create_by_cql(cql)
+    doc1.save
+    assert doc1.errors.empty?
+
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_CDS_UsingCommon.cql")
+    doc = Document.find_or_create_by_cql(cql)
+    assert doc.errors.empty?
+
+   
   end
 
   test "should be able to retrieved dependencies" do
-    assert false
+    
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_Common.cql")
+    doc1 = Document.find_or_create_by_cql(cql)
+    doc1.save
+    assert doc1.errors.empty?
+
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_CDS_UsingCommon.cql")
+    doc = Document.find_or_create_by_cql(cql)
+    assert doc.errors.empty?
+
+    assert_equal doc.find_dependencies.count() , 1, "should have one dependency"
+    assert_equal doc1.id , doc.find_dependencies.first.id, "Dep id should match"
   end
 
   test "should be able to find dependent documents" do 
-    assert false
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_Common.cql")
+    doc1 = Document.find_or_create_by_cql(cql)
+    doc1.save
+    assert doc1.errors.empty?
+
+    cql = File.read("test/fixtures/cql_documents/ChlamydiaScreening_CDS_UsingCommon.cql")
+    doc = Document.find_or_create_by_cql(cql)
+    assert doc.errors.empty?
+
+    assert_equal doc1.find_dependents.count() , 1, "Should have 1 dependent"
+    assert_equal doc.id , doc1.find_dependents.first.id, "Dependent id whould match"
   end
 
 
